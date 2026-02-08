ef5492d20f7b73e780f62a48f63cbceb382d77b7
sonam wangdus <144471374+WangdusGitHub@users.noreply.github.com>
2026-02-08 13:14:50 +0530

    added comments in server.js

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 const http = require('http');
+// adding fs for reading the files
 const fs = require('fs');
-const path = require('path');   
+
+//setting the PORT to 3000
 const PORT = 3000;
 
+// instead of writing the same code multiple times, i made a function for rendering the page according to user request;
 const showPage = (path, res, statusCode) => {
-
+    // reading the file
     fs.readFile(path, (err, data) => {
+        // handling error
         if(err) {
             res.writeHead(500);
             res.end("Server Error")
             return;
         }
+        // setting status code and content-type
         res.writeHead(statusCode, {'content-type': 'text/html'})
+        //writting the data in webpage
         res.write(data);
         res.end();
     })
 }
 
+// Creating the server
 const server = http.createServer((req, res) => {
 
+    // sending CSS file to client
     if(req.url === '/styles/style.css') {
         fs.readFile('./styles/style.css', (err, data) => {
             if(err) {
@@ -33,6 +41,7 @@ const server = http.createServer((req, res) => {
         return;
     }
 
+    // sending Laundry image to client
     if(req.url === '/images/laundry-img-01.png') {
         fs.readFile('./images/laundry-img-01.png', (err, data) => {
             if(err) {
@@ -46,7 +55,7 @@ const server = http.createServer((req, res) => {
         return;
     }
 
-
+    // Handling url
     if(req.url === '/' || req.url === '/home') {
         showPage('./pages/home.html', res, 200);
     } else if(req.url === '/about-us') {
@@ -58,6 +67,7 @@ const server = http.createServer((req, res) => {
     }
 });
 
+// listening to the server
 server.listen(PORT, () => {
     console.log(`Server is up and running at http://localhost:${PORT}`)
 });
